0b04dbbf39c1cb3e71c2f55b5b385b945864cc5a
Szilard Vig <vigszilard@yahoo.com>
2023-12-16 15:15:17 +0200

    Lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,28 @@
-﻿namespace Vig_Szilard_Lab7;
+﻿using System;
+using Vig_Szilard_Lab7.Data;
+using System.IO;
+
+namespace Vig_Szilard_Lab7;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+
+    public App()
 	{
 		InitializeComponent();
 
---
+++ Models/ShopList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Vig_Szilard_Lab7.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [MaxLength(250), Unique]
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
